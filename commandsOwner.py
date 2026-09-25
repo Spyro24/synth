986cc7666345beac83b3bcd69a7301eb4ccf2167
Spyro24 <72081109+Spyro24@users.noreply.github.com>
2026-09-25 23:31:20 +0200

    Add forceStatsSave function for saving stats

--- commandsOwner.py
+++ commandsOwner.py
@@ -7,3 +7,15 @@ def restart(args, bot, params: dict, helpStr=False):
             bot.flags.add("restart")
         else:
             bot.sendMessage(params["channel"], "Missing Permisions")
+
+def forceStatsSave(args, bot, params: dict, helpStr=False):
+    if helpStr:
+        return "`/forcestatssave` - forcing the stats save (will not influence the saving cycle)"
+    else:
+        if params["author"] == params["env"]["ownerId"]:
+            bot.log("Saving stats ...")
+            with open("stats.json", "w", encoding="utf-8") as f:
+                json.dump(bot.stats, f, ensure_ascii=False, indent=4)
+            bot.log("Stats saved")
+        else:
+            bot.sendMessage(params["channel"], "Missing Permisions")
